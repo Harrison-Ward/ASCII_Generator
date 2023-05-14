a39a21bd01e6a1cad235b14b87a6e6623fb481d4
Harrison Ward <hward4116@gmail.com>
2023-05-14 14:21:06 -0400

    minor bux fix to condtional statement

--- main/file_util.py
+++ main/file_util.py
@@ -4,5 +4,5 @@ from secret import path
 print(path)
 for file in os.listdir(path):
     fname, ftype = file.split('.')
-    if fname is not None and ftype != 'jpg':
-        file = os.rename(file, f"{fname}.jpg")
+    if fname != '' and ftype != 'jpeg':
+        file = os.rename(f'{path}/{file}', f"{path}/{fname}.jpeg")
