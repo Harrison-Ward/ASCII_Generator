e81573f1fa4334a82d2d518f98540d4207a9ce97
Harrison Ward <hward4116@gmail.com>
2023-05-18 14:42:12 -0400

    added image inversion and negation options to the ascii function

--- main/ascii_convolution.py
+++ main/ascii_convolution.py
@@ -7,8 +7,9 @@ import tensorflow as tf
 path = '/Users/harrisonward/Desktop/CS/Git/pixelator/assets'
 
 # convert grey scale to dict to speed up look up
-gscale1 = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
-gs_dict = {index:value for (index, value) in enumerate(gscale1)}
+gscale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+gs_dict = {index:value for (index, value) in enumerate(gscale)}
+gs_dict_inv = {index:value for (index, value) in enumerate(reversed(gscale))}
 
 def avg_brightness(image_array):
     try:
@@ -89,53 +90,56 @@ def to_ascii(image, n_cols, scale, output):
     return output
 
 @timer
-def ascii_convl(image, kernel_size, output):
+def ascii_convl(image, kernel_size, output, invert:bool, negative:bool):
     # take in the image and transform it into an array
-    print('loading array')
-    image_array = np.flip(convert_to_gs(np.array(image)), axis=0)
+    image_array = convert_to_gs(np.array(image))
+
+    if invert:
+        if image_array.shape[1] > image_array.shape[0]:
+            print('Flipped')
+            image_array = image_array.T
 
     # reshape the image so it is a valid 4D tensor for pooling
-    print('reshape array')
     image_array = image_array.reshape(1, image_array.shape[0], image_array.shape[1], 1)
 
     # average a neighborhood of pixels to get the luminosity of each tile
-    print('pooling')
     avg_pool_2d = tf.keras.layers.AveragePooling2D(pool_size=(kernel_size, kernel_size), strides=None, padding='valid')
     luminosity_array = np.asarray(avg_pool_2d(image_array))
 
     # map luminosity values to chars in grayscale list
-    print('mapping')
-    char_map = lambda i: gs_dict[int((i*69)/255)]
+    if negative:
+        char_map = lambda i: gs_dict_inv[int((i*69)/255)]
+    else:
+        char_map = lambda i: gs_dict[int((i*69)/255)]
+    
     mapping_function = np.vectorize(char_map)    
     ascii_array = np.squeeze(mapping_function(luminosity_array))
 
     # return the ascii array as text
-    print('saving')
     return np.savetxt(f'output/{output}', ascii_array, fmt='%c')
 
 
-def main(n_cols, scale):
-    # load images
+def main(kernel_size, invert=None, negative=None):
+    # load and lazily store images
     images = []
     for file in os.listdir(path):
         fname, ftype = file.split('.')
         if fname != '' and ftype == 'jpeg':
             images.append((Image.open(f'{path}/{file}'), fname))
-            print(f'File: {file}\nShape: {images[-1][0].size}')
     
-    ascii_convl(images[0][0], 5, 'aaa.txt')
-    # for i, image_tuple in enumerate(images):
-    #     print('-'*100)
-    #     print(f'Image {i + 1} of {len(images)} loading:')
-    #     print(f'{image_tuple[1]}, rendering...')
-    #     to_ascii(image_tuple[0], n_cols, scale, f'{image_tuple[1]}_ascii.txt')
-    #     print(f'ASCII art written to output/{image_tuple[1]}_ascii.txt')
+
+    for i, image_tuple in enumerate(images):
+        print('-'*100)
+        print(f'Image {i + 1} of {len(images)} loading:')
+        print(f'{image_tuple[1]}, of size {image_tuple[0].size} rendering...')
+        ascii_convl(image_tuple[0], kernel_size, f'{image_tuple[1]}_ascii.txt', invert, negative)
+        print(f'ASCII art written to output/{image_tuple[1]}_ascii.txt')
 
 
 
 
 if __name__ == '__main__':
-    main(600, 0.35)
+    main(25, invert=True, negative=True)
 
 
 
