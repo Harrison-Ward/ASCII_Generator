4cdcf7e8d8f22bb4e6ef20e1962e00b6f166c67a
Harrison Ward <hward4116@gmail.com>
2023-05-18 14:47:23 -0400

    deleted the old version of the ascii generation function

--- main/ascii_convolution.py
+++ main/ascii_convolution.py
@@ -6,8 +6,10 @@ import tensorflow as tf
 
 path = '/Users/harrisonward/Desktop/CS/Git/pixelator/assets'
 
-# convert grey scale to dict to speed up look up
+
 gscale = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
+
+# convert grey scale to dict to speed up look up, invert it for negative images
 gs_dict = {index: value for (index, value) in enumerate(gscale)}
 gs_dict_inv = {index: value for (index, value) in enumerate(reversed(gscale))}
 
@@ -34,68 +36,7 @@ def timer(func):
 
 
 @timer
-def to_ascii(image, n_cols, scale, output):
-
-    f = open(f'main/output/{output}', 'w')
-
-    image_array = np.flip(convert_to_gs(np.array(image)), axis=0)
-
-    W, H = image_array.shape
-
-    if W > H:
-        image_array = np.flip(image_array, axis=0).T
-
-    # transpose wide images
-
-    # tile width
-    w = W / n_cols
-
-    # tile height
-    h = w / scale
-
-    n_rows = int(H/h)
-
-    for i in range(n_rows):
-        line = ''
-
-        # pixel number times tile height
-        y1 = int(i * h)
-
-        # pixel one to the right
-        # could probably add a % to handle the wrap around
-        y2 = int((i+1)*h)
-
-        if i == n_rows - 1:
-            y2 = H
-
-        for j in range(n_cols):
-            # x tile is the pixel number times the tile width
-            x1 = int(j*w)
-            x2 = int((j+1)*w)
-
-            if i == n_cols - 1:
-                x2 = W
-
-            # slice the image array to get the tile
-            tile = image_array[x1:x2, y1:y2]
-
-            # get the average brightness of the tile
-            avg = avg_brightness(tile)
-
-            # look up and append the char
-            line += gs_dict[int((avg*69)/255)]
-
-        # write each line to output
-        f.write(line + '\n')
-
-    # cleanup
-    f.close()
-
-    return output
-
-
-@timer
-def ascii_convl(image, kernel_size, output, invert: bool, negative: bool):
+def ascii_conv(image, kernel_size, output, invert: bool, negative: bool):
     # take in the image and transform it into an array
     image_array = convert_to_gs(np.array(image))
 
@@ -138,7 +79,7 @@ def main(kernel_size, invert=None, negative=None):
         print('-'*100)
         print(f'Image {i + 1} of {len(images)} loading:')
         print(f'{image_tuple[1]}, of size {image_tuple[0].size} rendering...')
-        ascii_convl(image_tuple[0], kernel_size,
+        ascii_conv(image_tuple[0], kernel_size,
                     f'{image_tuple[1]}_ascii.txt', invert, negative)
         print(f'ASCII art written to output/{image_tuple[1]}_ascii.txt')
 
